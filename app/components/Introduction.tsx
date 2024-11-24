bbdd34f5aa722b7de717b09f030fb406ad8d92eb
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2024-11-24 21:02:27 +0100

    fix: Code ordered

--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -1,70 +1,71 @@
-"use client"
-import React, { useState } from 'react'
-import Image from 'next/image'
-import { TypeAnimation } from 'react-type-animation'
-import Link from 'next/link'
-import Modal from '../utils/Modal'
-import ContactForm from './Forms/ContactForm'
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import Link from "next/link";
+import Modal from "../utils/Modal";
+import ContactForm from "./Forms/ContactForm";
 
 const Introduction = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const handleOpenModal = () => setIsModalOpen(true);
-    const handleCloseModal = () => setIsModalOpen(false);
-
-    return (
-        <div className='z-20 w-full bg-darkBg/60'>
-            <div className='z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2'>
-            <div className=' flex justify-center items-center m-7 p-7 rounded-xl'>  
-                    <Image 
-                        src="/jorgevidovic.png" 
-                        alt='Jorge Vidovic' 
-                        height='500' 
-                        width='500'               
-                        priority
-                        className='border-2 border-white rounded-xl bg-black bg-opacity-30'
-                    />
-                </div>
-
-                <div className='flex flex-col justify-center max-w-md'>
-                    <h3 className='mb-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-10'>
-                        <TypeAnimation
-                            sequence=
-                            {[
-                                "Desarrollador de software", 1000
-                            ]}
-                            wrapper='span'
-                            speed={50}
-                            repeat={Infinity}
-                            className='font-bold'
-                        />
-                    </h3>
-                    <p className='mx-auto mb-2 text-xl md:mx-0 md:mb-8'>
-                        Desarrollo software a medida, abarcando todos las áreas del proyecto.
-                    </p>
-
-                    <div className='flex item-center justify-center gap-3 md:justify-start md:gap-10'>
-                        <Link
-                            href="/projects"
-                            className='px-3 py-2 transition-all border-2 cursor-pointer text-md w-fit rounded-xl hover:shadow-xl hover:shadow-white/50' >
-                            Proyectos
-                        </Link>
-
-                        <button
-                            onClick={handleOpenModal}
-                            className='px-3 py-2 transition-all bg-secondary border-2 cursor-pointer text-md w-fit rounded-xl hover:shadow-xl hover:shadow-white/50'>
-                            Contáctame
-                        </button>
-
-                        <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-                            <h2 className="text-2xl font-bold mb-4 text-black">Formulario de Contacto</h2>
-                            <ContactForm />
-                        </Modal>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="z-20 w-full bg-darkBg/60">
+      <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2">
+        <div className=" flex justify-center items-center m-7 p-7 rounded-xl">
+          <Image
+            src="/jorgevidovic.png"
+            alt="Jorge Vidovic"
+            height="500"
+            width="500"
+            priority
+            className="border-2 border-white rounded-xl bg-black bg-opacity-30"
+          />
         </div>
-    )
-}
 
-export default Introduction
+        <div className="flex flex-col justify-center max-w-md">
+          <h3 className="mb-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-10">
+            <TypeAnimation
+              sequence={["Desarrollador de software", 1000]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+              className="font-bold"
+            />
+          </h3>
+          <p className="mx-auto mb-2 text-xl md:mx-0 md:mb-8">
+            Desarrollo software a medida, abarcando todos las áreas del
+            proyecto.
+          </p>
+
+          <div className="flex item-center justify-center gap-3 md:justify-start md:gap-10">
+            <Link
+              href="/projects"
+              className="px-3 py-2 transition-all border-2 cursor-pointer text-md w-fit rounded-xl hover:shadow-xl hover:shadow-white/50"
+            >
+              Proyectos
+            </Link>
+
+            <button
+              onClick={handleOpenModal}
+              className="px-3 py-2 transition-all bg-secondary border-2 cursor-pointer text-md w-fit rounded-xl hover:shadow-xl hover:shadow-white/50"
+            >
+              Contáctame
+            </button>
+
+            <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+              <h2 className="text-2xl font-bold mb-4 text-black">
+                Formulario de Contacto
+              </h2>
+              <ContactForm />
+            </Modal>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Introduction;
--- data.tsx
+++ data.tsx
@@ -1,173 +1,187 @@
-import { BookText, CodeSquare, HomeIcon, UserRound, Linkedin, Youtube, Pencil, Computer, Speech, Github, Instagram } from "lucide-react";
+import {
+  BookText,
+  CodeSquare,
+  HomeIcon,
+  UserRound,
+  Linkedin,
+  Youtube,
+  Pencil,
+  Computer,
+  Speech,
+  Github,
+  Instagram,
+} from "lucide-react";
 import { FaTiktok } from "react-icons/fa";
 
 export const socialNetworks = [
-    {
-        id: 1,
-        logo: <Youtube size={30} strokeWidth={1} />,
-        src: "https://www.youtube.com/@JorgeVidovic",
-    },
-    {
-        id: 2,
-        logo: <Linkedin size={30} strokeWidth={1} />,
-        src: "https://www.linkedin.com/in/jorgevidovic/",
-    },
-    {
-        id: 3,
-        logo: <Instagram size={30} strokeWidth={1} />,
-        src: "https://www.instagram.com/jorgevidovic/",
-    },
-    {
-        id: 4,
-        logo: <Github size={30} strokeWidth={1} />,
-        src: "https://github.com/jorgevidovic",
-    },
-    {
-        id: 5,
-        logo: <FaTiktok />,
-        src: "https://www.tiktok.com/@jorgevidovic",
-    },
+  {
+    id: 1,
+    logo: <Youtube size={30} strokeWidth={1} />,
+    src: "https://www.youtube.com/@JorgeVidovic",
+  },
+  {
+    id: 2,
+    logo: <Linkedin size={30} strokeWidth={1} />,
+    src: "https://www.linkedin.com/in/jorgevidovic/",
+  },
+  {
+    id: 3,
+    logo: <Instagram size={30} strokeWidth={1} />,
+    src: "https://www.instagram.com/jorgevidovic/",
+  },
+  {
+    id: 4,
+    logo: <Github size={30} strokeWidth={1} />,
+    src: "https://github.com/jorgevidovic",
+  },
+  {
+    id: 5,
+    logo: <FaTiktok />,
+    src: "https://www.tiktok.com/@jorgevidovic",
+  },
 ];
 
-
 export const itemsNavbar = [
-    {
-        id: 1,
-        title: "Home",
-        icon: <HomeIcon size={25} color="#fff" strokeWidth={1} />,
-        link: "/",
-    },
-    {
-        id: 2,
-        title: "About",
-        icon: <UserRound size={25} color="#fff" strokeWidth={1} />,
-        link: "/about",
-    },
-    {
-        id: 3,
-        title: "Services",
-        icon: <BookText size={25} color="#fff" strokeWidth={1} />,
-        link: "/services",
-    },
-    {
-        id: 4,
-        title: "Projects",
-        icon: <CodeSquare size={25} color="#fff" strokeWidth={1} />,
-        link: "/projects",
-    },
-    {
-        id: 5,
-        title: "Testimonials",
-        icon: <Speech size={25} color="#fff" strokeWidth={1} />,
-        link: "/testimonials",
-    },
+  {
+    id: 1,
+    title: "Home",
+    icon: <HomeIcon size={25} color="#fff" strokeWidth={1} />,
+    link: "/",
+  },
+  {
+    id: 2,
+    title: "About",
+    icon: <UserRound size={25} color="#fff" strokeWidth={1} />,
+    link: "/about",
+  },
+  {
+    id: 3,
+    title: "Services",
+    icon: <BookText size={25} color="#fff" strokeWidth={1} />,
+    link: "/services",
+  },
+  {
+    id: 4,
+    title: "Projects",
+    icon: <CodeSquare size={25} color="#fff" strokeWidth={1} />,
+    link: "/projects",
+  },
+  {
+    id: 5,
+    title: "Testimonials",
+    icon: <Speech size={25} color="#fff" strokeWidth={1} />,
+    link: "/testimonials",
+  },
 ];
 
 export const dataAboutPage = [
-    {
-        id: 1,
-        title: "Desarrollador Full Stack",
-        subtitle: "Inteco Ingenieria Avanzada S.L.",
-        description: "Desarrollo Full Stack, abarcando todas las areas de desarrollo de producto, utilizando tecnologías .NET",
-        date: "Actualidad",
-    },
-    {
-        id: 2,
-        title: "Desarrollador Full Stack",
-        subtitle: "Freelancer - Vidovic Systems",
-        description: "Desarrollo Full Stack y diseño de producto basado en los requerimientos del cliente.",
-        date: "Actualidad",
-    },
-
-]
+  {
+    id: 1,
+    title: "Desarrollador Full Stack",
+    subtitle: "Inteco Ingenieria Avanzada S.L.",
+    description:
+      "Desarrollo Full Stack, abarcando todas las areas de desarrollo de producto, utilizando tecnologías .NET",
+    date: "Actualidad",
+  },
+  {
+    id: 2,
+    title: "CEO - Desarrollador Full Stack",
+    subtitle: "Vidovic Systems",
+    description:
+      "Desarrollo Full Stack y diseño de producto basado en los requerimientos del cliente.",
+    date: "Actualidad",
+  },
+];
 
 export const dataCounter = [
-    {
-        id: 0,
-        endCounter: 4,
-        text: "Años de experiencia",
-        lineRight: true,
-        lineRightMobile: true,
-    },
-    {
-        id: 1,
-        endCounter: 7,
-        text: "Clientes satisfechos",
-        lineRight: true,
-        lineRightMobile: true,
-    },
-    {
-        id: 2,
-        endCounter: 10,
-        text: "Proyectos finalizados",
-        lineRight: false,
-        lineRightMobile: false,
-    },
+  {
+    id: 0,
+    endCounter: 4,
+    text: "Años de experiencia",
+    lineRight: true,
+    lineRightMobile: true,
+  },
+  {
+    id: 1,
+    endCounter: 7,
+    text: "Clientes satisfechos",
+    lineRight: true,
+    lineRightMobile: true,
+  },
+  {
+    id: 2,
+    endCounter: 10,
+    text: "Proyectos finalizados",
+    lineRight: false,
+    lineRightMobile: false,
+  },
 ];
 
 export const serviceData = [
-    {
-        icon: <Pencil />,
-        title: "Frontend",
-        description: "Desarrollo profesional de interfaces web intuitivas y atractivas, centradas en la experiencia del usuario",
-    },
-    {
-        icon: <Computer />,
-        title: "Backend",
-        description: "Diseño y desarrollo de backends y API's, adaptadas a tus necesidades",
-    }
+  {
+    icon: <Pencil />,
+    title: "Frontend",
+    description:
+      "Desarrollo profesional de interfaces web intuitivas y atractivas, centradas en la experiencia del usuario",
+  },
+  {
+    icon: <Computer />,
+    title: "Backend",
+    description:
+      "Diseño y desarrollo de backends y API's, adaptadas a tus necesidades",
+  },
 ];
 
 export const dataPortfolio = [
-    {
-        id: 1,
-        title: "Ediciones Clío",
-        description:"",
-        image: "/edicionesclio.png",
-        urlGithub: "#!",
-        urlDemo: "#!",
-    },
-    {
-        id: 2,
-        title: "ADD2Q",
-        description:"",
-        image: "/add2q.png",
-        urlGithub: "#!",
-        urlDemo: "#!",
-    },
-    {
-        id: 3,
-        title: "Galistore",
-        description:"",
-        image: "/galistore.png",
-        urlGithub: "#!",
-        urlDemo: "#!",
-    },
+  {
+    id: 1,
+    title: "Ediciones Clío",
+    description: "",
+    image: "/edicionesclio.png",
+    urlGithub: "#!",
+    urlDemo: "#!",
+  },
+  {
+    id: 2,
+    title: "ADD2Q",
+    description: "",
+    image: "/add2q.png",
+    urlGithub: "#!",
+    urlDemo: "#!",
+  },
+  {
+    id: 3,
+    title: "Galistore",
+    description: "",
+    image: "/galistore.png",
+    urlGithub: "#!",
+    urlDemo: "#!",
+  },
 ];
 
 export const dataTestimonials = [
-    {
-        id: 1,
-        name: "Dr. Reyber Parra - Cronista de Maracibo",
-        description:
-            "El trabajo de Jorge me ha ayudado a hacer de nuestros proyectos tecnológicos mas escalables y sostenibles ¡Altamente recomendado!",
-    },
-    {
-        id: 2,
-        name: "Nestor Osechas - Director de Galistore",
-        description:
-            "El trabajo de Jorge con Galistore nos ha permitido sistematizar y gestionar nuestras ventas, optimizando la productividad de nuestro equipo de trabajo.",
-    },
-    {
-        id: 3,
-        name: "Brais Rodriguez - Cofundador del Mesón Delfín",
-        description:
-            "Jorge ha sido un gran aliado en el desarrollo de nuestra plataforma de ventas, su trabajo ha sido clave para el crecimiento de nuestro negocio.",
-    },
-    {
-        id: 4,
-        name: "Dr. Jorge Villasmil - Director de Multiverso Journal",
-        description:
-            "El trabajo de Jorge ha sido clave para la digitalización de nuestra revista, su trabajo ha sido de gran calidad y profesionalismo.",
-    },
+  {
+    id: 1,
+    name: "Dr. Reyber Parra - Cronista de Maracibo",
+    description:
+      "El trabajo de Jorge me ha ayudado a hacer de nuestros proyectos tecnológicos mas escalables y sostenibles ¡Altamente recomendado!",
+  },
+  {
+    id: 2,
+    name: "Nestor Osechas - Director de Galistore",
+    description:
+      "El trabajo de Jorge con Galistore nos ha permitido sistematizar y gestionar nuestras ventas, optimizando la productividad de nuestro equipo de trabajo.",
+  },
+  {
+    id: 3,
+    name: "Brais Rodriguez - Cofundador del Mesón Delfín",
+    description:
+      "Jorge ha sido un gran aliado en el desarrollo de nuestra plataforma de ventas, su trabajo ha sido clave para el crecimiento de nuestro negocio.",
+  },
+  {
+    id: 4,
+    name: "Dr. Jorge Villasmil - Director de Multiverso Journal",
+    description:
+      "El trabajo de Jorge ha sido clave para la digitalización de nuestra revista, su trabajo ha sido de gran calidad y profesionalismo.",
+  },
 ];
