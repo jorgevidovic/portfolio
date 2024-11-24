4d11b5eba148d3bdf8ce915b3e67988a3ec83b4d
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2024-11-24 20:59:20 +0100

    fix: Fixed Titles

--- app/(routes)/projects/page.tsx
+++ app/(routes)/projects/page.tsx
@@ -1,20 +1,21 @@
-import CircleImage from '@/app/components/CircleImage'
-import Container from '@/app/components/Container'
-import PortfolioBox from '@/app/components/PortfolioBox'
-import TransitionPage from '@/app/components/TransitionPage'
-import { dataPortfolio } from '@/data'
-import React from 'react'
+import CircleImage from "@/app/components/CircleImage";
+import Container from "@/app/components/Container";
+import PortfolioBox from "@/app/components/PortfolioBox";
+import TransitionPage from "@/app/components/TransitionPage";
+import { dataPortfolio } from "@/data";
+import React from "react";
 
 const ProjectsPage = () => {
   return (
     <Container>
       <TransitionPage />
-      <div className='flex flex-col justify-center h-full'>
-        <h1 className='text-2xl leading-tight text-center md:text-left md:text-5xl md:mt-10 font-bold md:px-20'>Proyectos</h1>
+      <div className="flex flex-col justify-center h-full mb-8">
+        <h1 className="text-2xl leading-tight text-center md:text-left md:text-5xl md:mt-10 font-bold md:px-20">
+          Proyectos
+        </h1>
       </div>
 
-      <div className='relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 sm:grid-cols-2 md:grid-cols-4'>
-
+      <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 sm:grid-cols-2 md:grid-cols-4">
         {dataPortfolio.map((data) => (
           <PortfolioBox key={data.id} data={data} />
         ))}
@@ -22,7 +23,7 @@ const ProjectsPage = () => {
 
       <CircleImage />
     </Container>
-  )
-}
+  );
+};
 
-export default ProjectsPage
+export default ProjectsPage;
--- app/(routes)/testimonials/page.tsx
+++ app/(routes)/testimonials/page.tsx
@@ -1,19 +1,18 @@
-"use client"
+"use client";
 
-import { Pagination } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { dataTestimonials } from '@/data';
-import TransitionPage from '@/app/components/TransitionPage';
-import CircleImage from '@/app/components/CircleImage';
-
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { dataTestimonials } from "@/data";
+import TransitionPage from "@/app/components/TransitionPage";
+import CircleImage from "@/app/components/CircleImage";
 
 const TestimonialsPage = () => {
   return (
     <>
       <TransitionPage />
-      <div className='flex flex-col justify-center h-lvh'>
+      <div className="flex flex-col justify-center h-lvh">
         <CircleImage />
-        <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5 font-bold">
+        <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5 font-bold  md:text-5xl">
           Testimonios
         </h1>
         <div className="flex items-center justify-center">
@@ -22,22 +21,22 @@ const TestimonialsPage = () => {
               breakpoints={{
                 320: {
                   slidesPerView: 1,
-                  spaceBetween: 15
+                  spaceBetween: 15,
                 },
               }}
               freeMode={true}
               pagination={{
-                clickable: true
+                clickable: true,
               }}
               modules={[Pagination]}
               className="h-[380px] md:h-[300px] w-[270px] md:w-[550px]"
             >
               {dataTestimonials.map(({ id, name, description }) => (
                 <SwiperSlide key={id}>
-                  <h4 className='text-center font-bold text-secondary'>{name}</h4>
-                  <div className="mt-5 text-center">
-                    {description}
-                  </div>
+                  <h4 className="text-center font-bold text-secondary">
+                    {name}
+                  </h4>
+                  <div className="mt-5 text-center">{description}</div>
                 </SwiperSlide>
               ))}
             </Swiper>
@@ -46,6 +45,6 @@ const TestimonialsPage = () => {
       </div>
     </>
   );
-}
+};
 
 export default TestimonialsPage;
