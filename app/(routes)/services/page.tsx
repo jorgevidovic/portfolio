aa4e19e01aac2e83aefca73e6c1d9e24b90ef984
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2025-11-17 16:44:46 +0100

    fix: We fixed the size of the boxes in services view

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -1,11 +1,11 @@
-"use client"
-import CircleImage from '@/app/components/CircleImage'
-import Container from '@/app/components/Container'
-import ContactForm from '@/app/components/Forms/ContactForm'
-import SliderService from '@/app/components/SliderService'
-import TransitionPage from '@/app/components/TransitionPage'
-import Modal from '@/app/utils/Modal'
-import React, { useState } from 'react'
+"use client";
+import CircleImage from "@/app/components/CircleImage";
+import Container from "@/app/components/Container";
+import ContactForm from "@/app/components/Forms/ContactForm";
+import SliderService from "@/app/components/SliderService";
+import TransitionPage from "@/app/components/TransitionPage";
+import Modal from "@/app/utils/Modal";
+import React, { useState } from "react";
 
 const ServicesPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -18,24 +18,32 @@ const ServicesPage = () => {
       <Container>
         <CircleImage />
         <div className="grid items-center justify-center max-w-5xl gap-6 mx-auto md:grid-cols-2">
-
-          <div className='max-w-[450px]'>
-            <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-tight text-center md:text-left md:mt-10 font-bold md:px-20 md:mb-8'>
+          <div className="max-w-[450px]">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-tight text-center md:text-left md:mt-10 font-bold md:px-20 md:mb-8">
               Servicios
             </h1>
-            <p className='mb-6 text-base sm:text-lg md:text-xl lg:text-2xl text-gray-300 text-center md:text-left leading-relaxed'>
-              Soy un desarrollador <span className='font-bold text-white'>Full Stack</span> con amplia experiencia en
-              el diseño y desarrollo tanto en el <span className='font-bold text-white'>Frontend</span> como en el <span className='font-bold text-white'>Backend</span>.
-              En el Frontend, manejo aspectos de diseño y desarrollo de interfaces,
-              mientras que en el Backend, me especializo en la arquitectura de sistemas,
-              lógica de negocio, y la gestión eficiente de bases de datos, entre otras áreas clave.
+            <p className="mb-6 text-base sm:text-lg md:text-lg lg:text-xl text-gray-300 text-center md:text-left leading-relaxed">
+              Soy un desarrollador{" "}
+              <span className="font-bold text-white">Full Stack</span> con
+              amplia experiencia en el diseño y desarrollo tanto en el{" "}
+              <span className="font-bold text-white">Frontend</span> como en el{" "}
+              <span className="font-bold text-white">Backend</span>. En el
+              Frontend, manejo aspectos de diseño y desarrollo de interfaces,
+              mientras que en el Backend, me especializo en la arquitectura de
+              sistemas, lógica de negocio, y la gestión eficiente de bases de
+              datos, entre otras áreas clave.
             </p>
-            <button className='px-5 py-3 text-base sm:text-lg rounded-lg bg-secondary hover:bg-secondary/65 mx-auto md:mx-0 block font-medium transition-all hover:scale-105' onClick={handleOpenModal}>
+            <button
+              className="px-5 py-3 text-base sm:text-lg rounded-lg bg-secondary hover:bg-secondary/65 mx-auto md:mx-0 block font-medium transition-all hover:scale-105"
+              onClick={handleOpenModal}
+            >
               Contacta conmigo
             </button>
 
             <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-              <h2 className="text-2xl font-bold mb-4 text-black">Formulario de Contacto</h2>
+              <h2 className="text-2xl font-bold mb-4 text-black">
+                Formulario de Contacto
+              </h2>
               <ContactForm />
             </Modal>
           </div>
@@ -46,7 +54,7 @@ const ServicesPage = () => {
         </div>
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default ServicesPage
+export default ServicesPage;
--- app/components/SliderService.tsx
+++ app/components/SliderService.tsx
@@ -29,19 +29,19 @@ const SliderService = () => {
         clickable: true,
       }}
       modules={[Pagination]}
-      className="h-[400px] sm:h-[420px] md:h-[400px] w-[270px] md:w-[550px]"
+      className="h-[400px] sm:h-[420px] md:h-[360px] w-[270px] md:w-[550px]"
     >
       {serviceData.map((item, index) => (
         <SwiperSlide key={index} className="h-auto">
-          <div className="flex flex-col px-6 py-8 h-full min-h-[340px] sm:min-h-[360px] md:min-h-[340px] rounded-lg cursor-pointer bg-[rgba(65,47,123,0.15)] group hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300 hover:border-secondary border-2">
-            <div className="mb-4 text-5xl sm:text-6xl md:text-5xl text-secondary flex-shrink-0">
+          <div className="flex flex-col px-6 py-8 h-full min-h-[340px] sm:min-h-[360px] md:min-h-[300px] rounded-lg cursor-pointer bg-[rgba(65,47,123,0.15)] group hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300 hover:border-secondary border-2">
+            <div className="mb-4 text-5xl sm:text-6xl md:text-4xl text-secondary flex-shrink-0">
               {item.icon}
             </div>
             <div className="flex-1 flex flex-col">
-              <h3 className="mb-4 text-2xl sm:text-3xl md:text-2xl font-bold flex-shrink-0">
+              <h3 className="mb-3 text-2xl sm:text-3xl md:text-xl font-bold flex-shrink-0">
                 {item.title}
               </h3>
-              <p className="text-lg sm:text-xl md:text-lg leading-relaxed">
+              <p className="text-lg sm:text-xl md:text-sm leading-relaxed">
                 {item.description}
               </p>
             </div>
